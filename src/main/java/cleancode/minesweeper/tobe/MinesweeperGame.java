4bedf84071bf9a98c79092667007e78c9d3f767f
jsween5723 <jsween5723@gmail.com>
2025-05-27 13:14:03 +0900

    refactor: extract method

--- src/main/java/cleancode/minesweeper/tobe/MinesweeperGame.java
+++ src/main/java/cleancode/minesweeper/tobe/MinesweeperGame.java
@@ -9,12 +9,25 @@ public class MinesweeperGame {
     private static Integer[][] landMineCounts = new Integer[8][10];
     private static boolean[][] landMines = new boolean[8][10];
     private static int gameStatus = 0; // 0: 게임 중, 1: 승리, -1: 패배
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.out.println("지뢰찾기 게임 시작!");
-        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        Scanner scanner = new Scanner(System.in);
+        initializeBoard();
+        gameStart();
+    }
+
+    private static void gameStart() {
+        showStartMessage();
+        while (true) {
+            showBoard();
+            if (isGameOver()) break;
+            System.out.println();
+            Coordinates coordinates = getCoordinates();
+            getUserActionFor(coordinates);
+        }
+    }
+
+    private static void initializeBoard() {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 10; j++) {
                 board[i][j] = "□";
@@ -59,106 +72,159 @@ public class MinesweeperGame {
                 landMineCounts[i][j] = 0;
             }
         }
-        while (true) {
-            System.out.println("   a b c d e f g h i j");
-            for (int i = 0; i < 8; i++) {
-                System.out.printf("%d  ", i + 1);
-                for (int j = 0; j < 10; j++) {
-                    System.out.print(board[i][j] + " ");
-                }
-                System.out.println();
-            }
-            if (gameStatus == 1) {
-                System.out.println("지뢰를 모두 찾았습니다. GAME CLEAR!");
-                break;
-            }
-            if (gameStatus == -1) {
-                System.out.println("지뢰를 밟았습니다. GAME OVER!");
-                break;
-            }
-            System.out.println();
-            System.out.println("선택할 좌표를 입력하세요. (예: a1)");
-            String input = scanner.nextLine();
-            System.out.println("선택한 셀에 대한 행위를 선택하세요. (1: 오픈, 2: 깃발 꽂기)");
-            String input2 = scanner.nextLine();
-            char c = input.charAt(0);
-            char r = input.charAt(1);
-            int col;
-            switch (c) {
-                case 'a':
-                    col = 0;
-                    break;
-                case 'b':
-                    col = 1;
-                    break;
-                case 'c':
-                    col = 2;
-                    break;
-                case 'd':
-                    col = 3;
-                    break;
-                case 'e':
-                    col = 4;
-                    break;
-                case 'f':
-                    col = 5;
-                    break;
-                case 'g':
-                    col = 6;
-                    break;
-                case 'h':
-                    col = 7;
-                    break;
-                case 'i':
-                    col = 8;
-                    break;
-                case 'j':
-                    col = 9;
-                    break;
-                default:
-                    col = -1;
-                    break;
-            }
-            int row = Character.getNumericValue(r) - 1;
-            if (input2.equals("2")) {
-                board[row][col] = "⚑";
-                boolean open = true;
-                for (int i = 0; i < 8; i++) {
-                    for (int j = 0; j < 10; j++) {
-                        if (board[i][j].equals("□")) {
-                            open = false;
-                        }
-                    }
-                }
-                if (open) {
-                    gameStatus = 1;
-                }
-            } else if (input2.equals("1")) {
-                if (landMines[row][col]) {
-                    board[row][col] = "☼";
-                    gameStatus = -1;
-                    continue;
-                } else {
-                    open(row, col);
-                }
-                boolean open = true;
-                for (int i = 0; i < 8; i++) {
-                    for (int j = 0; j < 10; j++) {
-                        if (board[i][j].equals("□")) {
-                            open = false;
-                        }
-                    }
-                }
-                if (open) {
-                    gameStatus = 1;
-                }
-            } else {
-                System.out.println("잘못된 번호를 선택하셨습니다.");
-            }
+    }
+
+
+    private static void getUserActionFor(Coordinates coordinates) {
+        String input2 = getUserActionInput();
+        if (input2.equals("2")) {
+            flagOn(coordinates);
+        } else if (input2.equals("1")) {
+            openAt(coordinates);
+        } else {
+            showInvalidInputError();
         }
     }
 
-    private static void open(int row, int col) {
+    private static void showInvalidInputError() {
+        System.out.println("잘못된 번호를 선택하셨습니다.");
+    }
+
+    private static String getUserActionInput() {
+        System.out.println("선택한 셀에 대한 행위를 선택하세요. (1: 오픈, 2: 깃발 꽂기)");
+        String input2 = scanner.nextLine();
+        return input2;
+    }
+
+
+    private static void openAt(Coordinates coordinates) {
+        if (landMines[coordinates.row()][coordinates.col()]) {
+            board[coordinates.row()][coordinates.col()] = "☼";
+            gameStatus = -1;
+            return;
+        } else {
+            openAt(coordinates.row(), coordinates.col());
+        }
+        boolean open = true;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (board[i][j].equals("□")) {
+                    open = false;
+                }
+            }
+        }
+        if (open) {
+            gameStatus = 1;
+        }
+    }
+
+    private static void flagOn(Coordinates coordinates) {
+        board[coordinates.row()][coordinates.col()] = "⚑";
+        boolean open = true;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (board[i][j].equals("□")) {
+                    open = false;
+                }
+            }
+        }
+        if (open) {
+            gameStatus = 1;
+        }
+    }
+
+    private static Coordinates getCoordinates() {
+        String input = getCoordinatesInput();
+        char c = input.charAt(0);
+        int col;
+        switch (c) {
+            case 'a':
+                col = 0;
+                break;
+            case 'b':
+                col = 1;
+                break;
+            case 'c':
+                col = 2;
+                break;
+            case 'd':
+                col = 3;
+                break;
+            case 'e':
+                col = 4;
+                break;
+            case 'f':
+                col = 5;
+                break;
+            case 'g':
+                col = 6;
+                break;
+            case 'h':
+                col = 7;
+                break;
+            case 'i':
+                col = 8;
+                break;
+            case 'j':
+                col = 9;
+                break;
+            default:
+                col = -1;
+                break;
+        }
+        char r = input.charAt(1);
+        int row = Character.getNumericValue(r) - 1;
+        Coordinates result = new Coordinates(col, row);
+        return result;
+    }
+
+    private static String getCoordinatesInput() {
+        System.out.println("선택할 좌표를 입력하세요. (예: a1)");
+        String input = scanner.nextLine();
+        return input;
+    }
+
+    private record Coordinates(int col, int row) {
+    }
+
+    private static boolean isGameOver() {
+        if (gameStatus == 1) {
+            showGameClearMessage();
+            return true;
+        }
+        if (gameStatus == -1) {
+            showGameOverMessage();
+            return true;
+        }
+        return false;
+    }
+
+    private static void showGameOverMessage() {
+        System.out.println("지뢰를 밟았습니다. GAME OVER!");
+    }
+
+    private static void showGameClearMessage() {
+        System.out.println("지뢰를 모두 찾았습니다. GAME CLEAR!");
+    }
+
+    private static void showBoard() {
+        System.out.println("   a b c d e f g h i j");
+        for (int i = 0; i < 8; i++) {
+            System.out.printf("%d  ", i + 1);
+            for (int j = 0; j < 10; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void showStartMessage() {
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        System.out.println("지뢰찾기 게임 시작!");
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    }
+
+    private static void openAt(int row, int col) {
         if (row < 0 || row >= 8 || col < 0 || col >= 10) {
             return;
         }
@@ -174,14 +240,14 @@ public class MinesweeperGame {
         } else {
             board[row][col] = "■";
         }
-        open(row - 1, col - 1);
-        open(row - 1, col);
-        open(row - 1, col + 1);
-        open(row, col - 1);
-        open(row, col + 1);
-        open(row + 1, col - 1);
-        open(row + 1, col);
-        open(row + 1, col + 1);
+        openAt(row - 1, col - 1);
+        openAt(row - 1, col);
+        openAt(row - 1, col + 1);
+        openAt(row, col - 1);
+        openAt(row, col + 1);
+        openAt(row + 1, col - 1);
+        openAt(row + 1, col);
+        openAt(row + 1, col + 1);
     }
 
 }
