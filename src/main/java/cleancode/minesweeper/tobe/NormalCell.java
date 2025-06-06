8171319517ee2f40bb2b83825c3f1c58bbb828a9
jsween5723 <gusckd57@naver.com>
2025-06-06 22:18:39 +0900

    feat: NormalCell 기능 추가

--- src/main/java/cleancode/minesweeper/tobe/NormalCell.java
+++ src/main/java/cleancode/minesweeper/tobe/NormalCell.java
@@ -1,14 +1,20 @@
 package cleancode.minesweeper.tobe;
 
-public class NormalCell extends Cell{
+public class NormalCell extends Cell {
+    private boolean opened = false;
+    private boolean flagged = false;
+
     @Override
     void open() {
-
+        if (opened) throw new IllegalStateException("이미 열린 셀은 열 수 없습니다.");
+        if (flagged) throw new IllegalStateException("플래그가 달린 셀은 열 수 없습니다.");
+        opened = true;
     }
 
     @Override
     void toggleFlag() {
-
+        if (opened) throw new IllegalStateException("열린 셀은 플래그를 달 수 없습니다.");
+        flagged = !flagged;
     }
 
     @Override
@@ -18,16 +24,21 @@ public class NormalCell extends Cell{
 
     @Override
     boolean canAutoOpen() {
-        return false;
+        return !opened && !flagged;
     }
 
     @Override
     boolean isOpened() {
-        return false;
+        return opened;
     }
 
     @Override
     boolean isFlagged() {
-        return false;
+        return flagged;
+    }
+
+    @Override
+    boolean isCleared() {
+        return opened && !flagged;
     }
 }
