810f8dfb42709bc91603185f45eb9fbe710d2d66
jsween5723 <jsween5723@gmail.com>
2025-06-10 17:41:46 +0900

    feat: Board toggleFlag 위임로직 추가

--- src/main/java/cleancode/minesweeper/tobe/minesweeper/board/Coordinate.java
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/Coordinate.java
@@ -3,6 +3,10 @@ package cleancode.minesweeper.tobe.minesweeper.board;
 import java.util.Random;
 
 public record Coordinate(int row, int column) {
+    public Coordinate {
+        if (!isNotMinus()) throw new IllegalArgumentException("잘못된 번호를 선택하셨습니다.");
+    }
+
     boolean isNotMinus() {
         return row >= 0 && column >= 0;
     }
