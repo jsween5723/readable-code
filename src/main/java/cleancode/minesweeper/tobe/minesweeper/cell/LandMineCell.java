3468f4e5e6e4bbfc86ff7aea2650cb410a1ad7e7
jsween5723 <jsween5723@gmail.com>
2025-06-10 18:57:20 +0900

    docs: 불필요 요구사항 제거

--- src/main/java/cleancode/minesweeper/tobe/minesweeper/cell/LandMineCell.java
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/cell/LandMineCell.java
@@ -3,7 +3,6 @@ package cleancode.minesweeper.tobe.minesweeper.cell;
 /**
  * 1. 지뢰 셀은 열렸을 때 지뢰표시를 해야한다.
  * 2. 지뢰 셀은 지뢰다.
- * 3. 지뢰 셀은 열렸을 때, 주변 셀을 열 수 없다.
  * 4. 지뢰 셀의 클리어 조건은 깃발이 꽂힌 상태다.
  */
 class LandMineCell extends Cell {
